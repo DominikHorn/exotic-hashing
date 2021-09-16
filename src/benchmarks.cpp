c35c3962ccc893a7165563e85038fff0d9dced79
Dominik Horn <dominik.horn@tum.de>
2021-09-16 16:31:50 +0200

    remove duplicate MapOMPHF benchmark target

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -150,8 +150,6 @@ using SimpleHollowTrie = exotic_hashing::SimpleHollowTrie<Data, exotic_hashing::
 BM(SimpleHollowTrie);
 using HollowTrie = exotic_hashing::HollowTrie<Data, exotic_hashing::support::FixedBitConverter<Data>>;
 BM(HollowTrie);
-using MapOMPHF = exotic_hashing::MapOMPHF<Data>;
-BM(MapOMPHF);
 using MWHC = exotic_hashing::MWHC<Data>;
 BM(MWHC);
 using CompressedMWHC = exotic_hashing::CompressedMWHC<Data>;
