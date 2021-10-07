8a6c1dd7bc06f2ef752bb0ff386379cb86f96aee
Dominik Horn <dominik.horn@tum.de>
2021-10-07 17:21:37 +0200

    prepare MWHC code for cache oblivious construction

--- include/mwhc.hpp
+++ include/mwhc.hpp
@@ -24,14 +24,210 @@
 #include "include/reduction.hpp"
 
 namespace exotic_hashing {
-   template<class Data>
+   namespace support {
+      template<class Data, class Hasher>
+      class HyperGraph {
+         class Vertex {
+            /// implemented using modified XOR-trick. Original from:
+            /// https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=6824443
+            std::uint64_t data = 0;
+
+            static constexpr forceinline size_t edges_width() {
+               return 48;
+            }
+
+            static constexpr forceinline size_t edges_mask() {
+               return (0x1LLU << edges_width()) - 1;
+            }
+
+           public:
+            /// degree mustn't be larger than 255
+            forceinline size_t degree() const {
+               return data >> edges_width();
+            }
+
+            /// edges are represented as 48 bit integers
+            forceinline void add_edge(const size_t edge) {
+               // increment degree & xor edge ontop of data (add it)
+               data = ((degree() + 1) << edges_width()) | ((data ^ edge) & edges_mask());
+            }
+
+            /// edges are represented as 48 bit integers
+            forceinline void remove_edge(const size_t edge) {
+               // decrement degree & xor edge ontop of data (remove it)
+               data = ((degree() - 1) << edges_width()) | ((data ^ edge) & edges_mask());
+            }
+
+            /// edges are represented as 48 bit integers
+            forceinline size_t retrieve_last() const {
+               assert(degree() == 1);
+               return data & edges_mask();
+            }
+         };
+
+         std::vector<Vertex> vertices;
+
+         const std::vector<Data>& dataset;
+         const Hasher& hasher;
+
+        public:
+         HyperGraph(const std::vector<Data>& dataset, const Hasher& hasher, const size_t& N)
+            : vertices(N), dataset(dataset), hasher(hasher) {
+            // Construct random hypergraph using hasher
+            for (size_t i = 0; i < dataset.size(); i++) {
+               // TODO(dominik): remove these random cache accesses
+               const auto [h0, h1, h2] = hasher(dataset[i]);
+               vertices[h0].add_edge(i);
+               vertices[h1].add_edge(i);
+               vertices[h2].add_edge(i);
+            }
+         }
+
+         /**
+          * Peels the hypergraph as described Majewski, Bohdan S., et al. in "A
+          * family of perfect hashing methods." The Computer Journal 39.6
+          * (1996): 547-554.
+          *
+          * @return stack containing the acyclicity check traversal order
+          *   (reversed since stack is FILO), or an empty stack if the hypegraph
+          *   contained a cycle
+          */
+         std::stack<size_t> peel() {
+            // TODO(dominik): implement cache oblivious peeling, i.e., remove random access to vertices in
+            // vertex struct!
+            std::stack<size_t> res;
+
+            // 1. Recursively peel all vertices if possible (without recursion to avoid stack overflows)
+            for (size_t vertex_index = 0; vertex_index < vertices.size(); vertex_index++) {
+               std::queue<size_t> next_vertices;
+               next_vertices.push(vertex_index);
+
+               while (!next_vertices.empty()) {
+                  // TODO(dominik): eliminiate (if possible) this random cache access
+                  auto& vertex = vertices[next_vertices.front()];
+                  next_vertices.pop();
+
+                  // Only vertices with degree 1 are peelable
+                  if (vertex.degree() != 1)
+                     continue;
+
+                  // Peel last edge from this vertex
+                  const auto edge = vertex.retrieve_last();
+                  const auto [h0, h1, h2] = hasher(dataset[edge]);
+                  // TODO(dominik): eliminiate (if ) these random cache accesses
+                  vertices[h0].remove_edge(edge);
+                  vertices[h1].remove_edge(edge);
+                  vertices[h2].remove_edge(edge);
+
+                  // Keep track of edge removal order
+                  res.push(edge);
+
+                  // Attempt to peel adjacent vertices next
+                  next_vertices.push(h0);
+                  next_vertices.push(h1);
+                  next_vertices.push(h2);
+               }
+            }
+
+            // 2. Check if there are any edges left, i.e., any vertex has degree > 0. If so, the
+            //    acyclicity check has failed
+            for (const auto& vertex : vertices)
+               if (vertex.degree() > 0)
+                  return {};
+
+            return res;
+         }
+      };
+
+      template<class Data>
+      class Hasher {
+         const hashing::AquaHash<Data> hashfn;
+         hashing::reduction::FastModulo<Data> reducer;
+         std::uint64_t s0, s1, s2;
+
+         void seed_hashfns() {}
+
+         forceinline __m128i make_seed(const std::uint64_t lower, const std::uint64_t upper = 0) const {
+            return _mm_setr_epi8(static_cast<char>((upper >> 56) & 0xFF), static_cast<char>((upper >> 48) & 0xFF),
+                                 static_cast<char>((upper >> 40) & 0xFF), static_cast<char>((upper >> 32) & 0xFF),
+                                 static_cast<char>((upper >> 24) & 0xFF), static_cast<char>((upper >> 16) & 0xFF),
+                                 static_cast<char>((upper >> 8) & 0xFF), static_cast<char>((upper >> 0) & 0xFF),
+
+                                 static_cast<char>((lower >> 56) & 0xFF), static_cast<char>((lower >> 48) & 0xFF),
+                                 static_cast<char>((lower >> 40) & 0xFF), static_cast<char>((lower >> 32) & 0xFF),
+                                 static_cast<char>((lower >> 24) & 0xFF), static_cast<char>((lower >> 16) & 0xFF),
+                                 static_cast<char>((lower >> 8) & 0xFF), static_cast<char>((lower >> 0) & 0xFF));
+         }
+
+        public:
+         explicit Hasher(const size_t& N) : reducer(N) {
+            // Randomly seed hash functions
+            std::random_device r;
+            std::default_random_engine rng(r());
+            std::uniform_int_distribution<std::uint64_t> dist(std::numeric_limits<std::uint64_t>::min(),
+                                                              std::numeric_limits<std::uint64_t>::max());
+            s0 = dist(rng);
+            do
+               s1 = dist(rng);
+            while (s1 == s0);
+            do
+               s2 = dist(rng);
+            while (s2 == s0 || s2 == s1);
+         }
+
+         ~Hasher() = default;
+         Hasher(const Hasher& other) : s0(other.s0), s1(other.s1), s2(other.s2), reducer(other.reducer.N) {}
+         Hasher& operator=(const Hasher& other) {
+            if (&other == this)
+               return *this;
+
+            s0 = other.s0;
+            s1 = other.s1;
+            s2 = other.s2;
+            reducer = hashing::reduction::FastModulo<Data>(other.reducer.N);
+
+            return *this;
+         }
+
+         Hasher(Hasher&& other) = delete;
+         Hasher& operator=(Hasher&& other) noexcept {
+            if (&other == this)
+               return *this;
+
+            s0 = other.s0;
+            s1 = other.s1;
+            s2 = other.s2;
+            reducer = hashing::reduction::FastModulo<Data>(other.reducer.N);
+
+            return *this;
+         }
+
+         forceinline std::tuple<size_t, size_t, size_t> operator()(const Data& d) const {
+            return std::make_tuple(reducer(hashfn(d, make_seed(s0))), //
+                                   reducer(hashfn(d, make_seed(s1))), //
+                                   reducer(hashfn(d, make_seed(s2))));
+         }
+      };
+
+   } // namespace support
+
+   template<class Data, class Hasher = support::Hasher<Data>, class HyperGraph = support::HyperGraph<Data, Hasher>>
    struct MWHC;
 
-   template<class Data>
-   struct CompactedMWHC {
+   template<class Data, class Hasher = support::Hasher<Data>, class HyperGraph = support::HyperGraph<Data, Hasher>>
+   class CompactedMWHC {
+      using MWHC = MWHC<Data, Hasher, HyperGraph>;
+      Hasher hasher;
+      hashing::reduction::FastModulo<std::uint64_t> mod_N;
+
+      sdsl::bit_vector_il<> bit_vec;
+      decltype(bit_vec)::rank_1_type bit_vec_rank;
+      sdsl::int_vector<> vertex_values;
+
+     public:
       explicit CompactedMWHC(const std::vector<Data>& dataset)
-         : hasher(MWHC<Data>::vertices_count(dataset.size())), mod_N(MWHC<Data>::vertices_count(dataset.size())) {
-         const MWHC<Data> mwhc(dataset);
+         : hasher(MWHC::vertices_count(dataset.size())), mod_N(MWHC::vertices_count(dataset.size())) {
+         const MWHC mwhc(dataset);
 
          // copy unchanged fields
          hasher = mwhc.hasher;
@@ -103,21 +299,20 @@ namespace exotic_hashing {
          return sizeof(hasher) + sizeof(mod_N) + sdsl::size_in_bytes(bit_vec) + sdsl::size_in_bytes(bit_vec_rank) +
             sdsl::size_in_bytes(vertex_values);
       }
-
-     private:
-      typename MWHC<Data>::Hasher hasher;
-      hashing::reduction::FastModulo<std::uint64_t> mod_N;
-
-      sdsl::bit_vector_il<> bit_vec;
-      decltype(bit_vec)::rank_1_type bit_vec_rank;
-      sdsl::int_vector<> vertex_values;
    };
 
-   template<class Data>
-   struct CompressedMWHC {
+   template<class Data, class Hasher = support::Hasher<Data>, class HyperGraph = support::HyperGraph<Data, Hasher>>
+   class CompressedMWHC {
+      using MWHC = MWHC<Data, Hasher, HyperGraph>;
+
+      Hasher hasher;
+      hashing::reduction::FastModulo<std::uint64_t> mod_N;
+      sdsl::int_vector<> vertex_values;
+
+     public:
       explicit CompressedMWHC(const std::vector<Data>& dataset)
-         : hasher(MWHC<Data>::vertices_count(dataset.size())), mod_N(MWHC<Data>::vertices_count(dataset.size())) {
-         const MWHC<Data> mwhc(dataset);
+         : hasher(MWHC::vertices_count(dataset.size())), mod_N(MWHC::vertices_count(dataset.size())) {
+         const MWHC mwhc(dataset);
 
          // copy unchanged fields
          hasher = mwhc.hasher;
@@ -156,14 +351,9 @@ namespace exotic_hashing {
       size_t byte_size() const {
          return sizeof(hasher) + sizeof(mod_N) + sdsl::size_in_bytes(vertex_values);
       }
-
-     private:
-      typename MWHC<Data>::Hasher hasher;
-      hashing::reduction::FastModulo<std::uint64_t> mod_N;
-      sdsl::int_vector<> vertex_values;
    };
 
-   template<class Data>
+   template<class Data, class Hasher, class HyperGraph>
    struct MWHC {
       explicit MWHC(const std::vector<Data>& dataset)
          : hasher(vertices_count(dataset.size())), mod_N(vertices_count(dataset.size())) {
@@ -242,189 +432,12 @@ namespace exotic_hashing {
          return std::ceil(overalloc * dataset_size);
       }
 
-      class Hasher {
-         const hashing::AquaHash<Data> hashfn;
-         hashing::reduction::FastModulo<Data> reducer;
-         std::uint64_t s0, s1, s2;
-
-         void seed_hashfns() {}
-
-         forceinline __m128i make_seed(const std::uint64_t lower, const std::uint64_t upper = 0) const {
-            return _mm_setr_epi8(static_cast<char>((upper >> 56) & 0xFF), static_cast<char>((upper >> 48) & 0xFF),
-                                 static_cast<char>((upper >> 40) & 0xFF), static_cast<char>((upper >> 32) & 0xFF),
-                                 static_cast<char>((upper >> 24) & 0xFF), static_cast<char>((upper >> 16) & 0xFF),
-                                 static_cast<char>((upper >> 8) & 0xFF), static_cast<char>((upper >> 0) & 0xFF),
-
-                                 static_cast<char>((lower >> 56) & 0xFF), static_cast<char>((lower >> 48) & 0xFF),
-                                 static_cast<char>((lower >> 40) & 0xFF), static_cast<char>((lower >> 32) & 0xFF),
-                                 static_cast<char>((lower >> 24) & 0xFF), static_cast<char>((lower >> 16) & 0xFF),
-                                 static_cast<char>((lower >> 8) & 0xFF), static_cast<char>((lower >> 0) & 0xFF));
-         }
-
-        public:
-         explicit Hasher(const size_t& N) : reducer(N) {
-            // Randomly seed hash functions
-            std::random_device r;
-            std::default_random_engine rng(r());
-            std::uniform_int_distribution<std::uint64_t> dist(std::numeric_limits<std::uint64_t>::min(),
-                                                              std::numeric_limits<std::uint64_t>::max());
-            s0 = dist(rng);
-            do
-               s1 = dist(rng);
-            while (s1 == s0);
-            do
-               s2 = dist(rng);
-            while (s2 == s0 || s2 == s1);
-         }
-
-         ~Hasher() = default;
-         Hasher(const Hasher& other) : s0(other.s0), s1(other.s1), s2(other.s2), reducer(other.reducer.N) {}
-         Hasher& operator=(const Hasher& other) {
-            if (&other == this)
-               return *this;
-
-            s0 = other.s0;
-            s1 = other.s1;
-            s2 = other.s2;
-            reducer = hashing::reduction::FastModulo<Data>(other.reducer.N);
-
-            return *this;
-         }
-
-         Hasher(Hasher&& other) = delete;
-         Hasher& operator=(Hasher&& other) noexcept {
-            if (&other == this)
-               return *this;
-
-            s0 = other.s0;
-            s1 = other.s1;
-            s2 = other.s2;
-            reducer = hashing::reduction::FastModulo<Data>(other.reducer.N);
-
-            return *this;
-         }
-
-         forceinline std::tuple<size_t, size_t, size_t> operator()(const Data& d) const {
-            return std::make_tuple(reducer(hashfn(d, make_seed(s0))), //
-                                   reducer(hashfn(d, make_seed(s1))), //
-                                   reducer(hashfn(d, make_seed(s2))));
-         }
-      };
-
-      class HyperGraph {
-         class Vertex {
-            /// implemented using modified XOR-trick. Original from:
-            /// https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=6824443
-            std::uint64_t data = 0;
-
-            constexpr forceinline size_t edges_width() const {
-               return 48;
-            }
-
-            constexpr forceinline size_t edges_mask() const {
-               return (0x1LLU << edges_width()) - 1;
-            }
-
-           public:
-            /// degree mustn't be larger than 255
-            forceinline size_t degree() const {
-               return data >> edges_width();
-            }
-
-            /// edges are represented as 48 bit integers
-            forceinline void add_edge(const size_t edge) {
-               // increment degree & xor edge ontop of data (add it)
-               data = ((degree() + 1) << edges_width()) | ((data ^ edge) & edges_mask());
-            }
-
-            /// edges are represented as 48 bit integers
-            forceinline void remove_edge(const size_t edge) {
-               // decrement degree & xor edge ontop of data (remove it)
-               data = ((degree() - 1) << edges_width()) | ((data ^ edge) & edges_mask());
-            }
-
-            /// edges are represented as 48 bit integers
-            forceinline size_t retrieve_last() {
-               assert(degree() == 1);
-               return data & edges_mask();
-            }
-         };
-
-         std::vector<Vertex> vertices;
-
-         const std::vector<Data>& dataset;
-         const Hasher& hasher;
-
-        public:
-         HyperGraph(const std::vector<Data>& dataset, const Hasher& hasher, const size_t& N)
-            : vertices(N), dataset(dataset), hasher(hasher) {
-            // Construct random hypergraph using hasher
-            for (size_t i = 0; i < dataset.size(); i++) {
-               const auto [h0, h1, h2] = hasher(dataset[i]);
-               vertices[h0].add_edge(i);
-               vertices[h1].add_edge(i);
-               vertices[h2].add_edge(i);
-            }
-         }
-
-         /**
-          * Peels the hypergraph as described Majewski, Bohdan S., et al. in "A
-          * family of perfect hashing methods." The Computer Journal 39.6
-          * (1996): 547-554.
-          *
-          * @return stack containing the acyclicity check traversal order
-          *   (reversed since stack is FILO), or an empty stack if the hypegraph
-          *   contained a cycle
-          */
-         std::stack<size_t> peel() {
-            std::stack<size_t> res;
-
-            // 1. Recursively peel all vertices if possible (without recursion to avoid stack overflows)
-            for (size_t vertex_index = 0; vertex_index < vertices.size(); vertex_index++) {
-               std::queue<size_t> next_vertices;
-               next_vertices.push(vertex_index);
-
-               while (!next_vertices.empty()) {
-                  auto& vertex = vertices[next_vertices.front()];
-                  next_vertices.pop();
-
-                  // Only vertices with degree 1 are peelable
-                  if (vertex.degree() != 1)
-                     continue;
-
-                  // Peel last edge from this vertex
-                  const auto edge = vertex.retrieve_last();
-                  const auto [h0, h1, h2] = hasher(dataset[edge]);
-                  vertices[h0].remove_edge(edge);
-                  vertices[h1].remove_edge(edge);
-                  vertices[h2].remove_edge(edge);
-
-                  // Keep track of edge removal order
-                  res.push(edge);
-
-                  // Attempt to peel adjacent vertices next
-                  next_vertices.push(h0);
-                  next_vertices.push(h1);
-                  next_vertices.push(h2);
-               }
-            }
-
-            // 2. Check if there are any edges left, i.e., any vertex has degree > 0. If so, the
-            //    acyclicity check has failed
-            for (const auto& vertex : vertices)
-               if (vertex.degree() > 0)
-                  return {};
-
-            return res;
-         }
-      };
-
       Hasher hasher;
       hashing::reduction::FastModulo<std::uint64_t> mod_N;
 
       std::vector<size_t> vertex_values;
 
-      friend CompressedMWHC<Data>;
-      friend CompactedMWHC<Data>;
+      friend CompressedMWHC<Data, Hasher, HyperGraph>;
+      friend CompactedMWHC<Data, Hasher, HyperGraph>;
    };
 } // namespace exotic_hashing
