2bd1f6f7566d3717c55787c32b6278da5d8bfe57
Dominik Horn <dominik.horn@tum.de>
2021-10-23 15:38:05 +0200

    refactor cmake names into variables

--- thirdparty/googletest.cmake
+++ thirdparty/googletest.cmake
@@ -1,10 +1,12 @@
 include(FetchContent)
 
+set(GOOGLETEST_CONTENT googletest)
+set(GOOGLETEST_LIBRARY gtest_main)
 FetchContent_Declare(
-  googletest
+  ${GOOGLETEST_CONTENT}
   URL https://github.com/google/googletest/archive/96f4ce02a3a78d63981c67acbd368945d11d7d70.zip
   )
 
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(googletest)
+FetchContent_MakeAvailable(${GOOGLETEST_CONTENT})
