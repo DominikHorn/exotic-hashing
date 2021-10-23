1fbfd8fb7405c08e238f59d8049b84f3d9cfe5d5
Dominik Horn <dominik.horn@tum.de>
2021-10-23 13:55:46 +0200

    upgrade learned_hashing dependency to latest version

--- thirdparty/learned_hashing.cmake
+++ thirdparty/learned_hashing.cmake
@@ -4,7 +4,7 @@ set(LEARNED_HASHING_LIBRARY learned-hashing)
 FetchContent_Declare(
   ${LEARNED_HASHING_LIBRARY}
   GIT_REPOSITORY git@github.com:DominikHorn/learned-hashing.git
-  GIT_TAG 15af2b2
+  GIT_TAG 235ef15
   )
 
 FetchContent_MakeAvailable(${LEARNED_HASHING_LIBRARY})
