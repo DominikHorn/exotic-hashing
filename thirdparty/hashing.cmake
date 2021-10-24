8cdcb39edfaff60079cb9c56bd10bf0648333e1d
Dominik Horn <dominik.horn@tum.de>
2021-10-24 17:18:24 +0200

    upgrade to latest hashing dependency version

--- thirdparty/hashing.cmake
+++ thirdparty/hashing.cmake
@@ -4,7 +4,7 @@ set(HASHING_LIBRARY hashing)
 FetchContent_Declare(
   ${HASHING_LIBRARY}
   GIT_REPOSITORY git@github.com:DominikHorn/hashing.git
-  GIT_TAG d16f3e2
+  GIT_TAG 943dac2
   )
 
 FetchContent_MakeAvailable(${HASHING_LIBRARY})
