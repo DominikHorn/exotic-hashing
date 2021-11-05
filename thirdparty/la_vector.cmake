af70e666be30f3780eecd3b38507cbc6b8c5784c
Dominik Horn <dominik.horn@tum.de>
2021-11-05 17:32:00 +0100

    upgrade la_vector dependency to fix compile warnings

--- thirdparty/la_vector.cmake
+++ thirdparty/la_vector.cmake
@@ -4,7 +4,7 @@ set(LA_VECTOR_LIBRARY la_vector)
 FetchContent_Declare(
   ${LA_VECTOR_LIBRARY}
   GIT_REPOSITORY https://github.com/DominikHorn/la_vector.git
-  GIT_TAG 5efa77b
+  GIT_TAG 50edf27
   )
 
 FetchContent_MakeAvailable(${LA_VECTOR_LIBRARY})
