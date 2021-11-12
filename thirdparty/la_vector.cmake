b64b89f1859ceeb37e6104302de88a6e70e3e250
Dominik Horn <dominik.horn@tum.de>
2021-11-12 12:08:47 +0100

    upgrade to newest la_vector version

--- thirdparty/la_vector.cmake
+++ thirdparty/la_vector.cmake
@@ -4,7 +4,7 @@ set(LA_VECTOR_LIBRARY la_vector)
 FetchContent_Declare(
   ${LA_VECTOR_LIBRARY}
   GIT_REPOSITORY https://github.com/DominikHorn/la_vector.git
-  GIT_TAG 50edf27
+  GIT_TAG cd0409e
   )
 
 FetchContent_MakeAvailable(${LA_VECTOR_LIBRARY})
