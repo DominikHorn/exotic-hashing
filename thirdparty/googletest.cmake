ab9cf518a9a9d971b7af5277b5b1aee74df1a786
Dominik Horn <dominik.horn@tum.de>
2021-11-12 12:08:38 +0100

    update google tests version

--- thirdparty/googletest.cmake
+++ thirdparty/googletest.cmake
@@ -4,9 +4,12 @@ set(GOOGLETEST_CONTENT googletest)
 set(GOOGLETEST_LIBRARY gtest_main)
 FetchContent_Declare(
   ${GOOGLETEST_CONTENT}
-  URL https://github.com/google/googletest/archive/96f4ce02a3a78d63981c67acbd368945d11d7d70.zip
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG 9ca071b
   )
 
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(${GOOGLETEST_CONTENT})
